49c09d7f3889146ed14c99086f41a12b21cad737
Will McCutchen <will@mccutch.org>
2007-06-09 17:10:12 +0000

    Added write_xml() function to pretty-print an ElementTree object

--- wrm/trunk/xmlutils.py
+++ wrm/trunk/xmlutils.py
@@ -12,12 +12,8 @@ from wrm.utils import exclude
 
 @wrm.decorators.cached
 def add_element(parent, tagname, attrs={}, children={}, text=None):
-    """
-    add_element(parent, tagname[, attrs[, children[, text]]]) -> ElementTree.Element
-
-    Returns an ElementTree.Element object which is guaranteed
-    to be unique, based on the given arguments.
-    """
+    """Returns an ElementTree.Element object which is guaranteed to be unique,
+    based on the given arguments."""
 
     forbiddenvalues = [None, '']
     attrs = exclude(attrs, values=forbiddenvalues)
@@ -46,8 +42,8 @@ def is_xml_fragment(data):
     return False
 
 def xml_escape(text):
-    """Encodes reserved and non-ascii chars in text as numerical
-    entity references.  Taken from ElementTree.SimpleXMLWriter."""
+    """Encodes reserved and non-ascii chars in text as numerical entity
+    references.  Taken from ElementTree.SimpleXMLWriter."""
 
     # reserved and non-ascii chars
     chars_to_escape = re.compile(eval(r'u"[&<>\"\u0080-\uffff]+"'))
@@ -62,12 +58,9 @@ def xml_escape(text):
 
     return re.sub(chars_to_escape, escape_entities, text).encode("ascii")
 
-
 def get_unique_element(root, tagname, attrs={}, children={}):
-    """
-    Returns the first element in root whose attributes
-    match those given in the attrs dictionary
-    """
+    """Returns the first element in root whose attributes match those given in
+    the attrs dictionary."""
     candidates = root.findall(tagname)
     for candidate in candidates:
         badflag = 0
@@ -82,3 +75,34 @@ def get_unique_element(root, tagname, attrs={}, children={}):
     for childname, value in children.items():
         SubElement(el, childname).text = value
     return el
+
+def write_xml(tree, outputpath, encoding='utf-8'):
+    assert isinstance(tree, ElementTree), 'xmlutils.write_xml() requires an \
+        instance of ElementTree to write to disk.'
+    
+    # add indentation to the given element
+    indent(tree.getroot())
+    
+    # write to disk
+    outfile = open(outputpath, 'w')
+    tree.write(outfile, encoding)
+    outfile.close()    
+
+def indent(elem, level=0):
+    """Adds whitespace to the given element and its children, which results
+    in pretty-printing when the element is actually output.
+    
+    From:  http://effbot.org/zone/element-lib.htm#prettyprint"""
+    i = "\n" + level*"  "
+    if len(elem):
+        if not elem.text or not elem.text.strip():
+            elem.text = i + "  "
+        for e in elem:
+            indent(e, level+1)
+            if not e.tail or not e.tail.strip():
+                e.tail = i + "  "
+        if not e.tail or not e.tail.strip():
+            e.tail = i
+    else:
+        if level and (not elem.tail or not elem.tail.strip()):
+            elem.tail = i
