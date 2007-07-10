8282bab25554679f01b813e27dcba0098ef925a6
Will McCutchen <will@mccutch.org>
2007-07-10 20:32:37 +0000

    Added doctests to improve documentation

--- wrm/trunk/formatter.py
+++ wrm/trunk/formatter.py
@@ -1,20 +1,72 @@
 import re
-from decorators import cachedmethod
+from decorators import cached
 
-class Formatter:
-    """A data formatter that operates on dicts.  Each value in the
-    input dict is formatted according to the format function defined
-    by the results of __get_format_func(key).  If no format function
-    is found, the default_formatter() function is used."""
+class Formatter(object):
+    """A Formatter takes a dict as input and returns a new dict as output
+    after "formatting" or processing each of the values in the input dict.
+    By default, the values are returned unchanged, but Formatters can be
+    subclassed to provide custom formatting methods for individual fields.
+    
+    This input will be used in all of the following examples.  Note that the
+    'age' field is a string, not an int.
+    
+    >>> input = dict(name='Will', age='25', sex='Male')
+    >>> input
+    {'age': '25', 'name': 'Will', 'sex': 'Male'}
+    
+    An instance of the Formatter base class will return an identical dict.
+    
+    >>> formatter = Formatter()
+    >>> formatter.format(input)
+    {'age': '25', 'name': 'Will', 'sex': 'Male'}
+    
+    The result is equivalent, but it is a fresh dict.
+    
+    >>> formatter.format(input) == input
+    True
+    >>> formatter.format(input) is input
+    False
+    
+
+    Sublcasses of Formatter can define specialized methods to format
+    individual fields.  This custom Formatter should return an int for the
+    age and just the first letter of the sex in the input dict.
+    
+    >>> class PersonFormatter(Formatter):
+    ...     def format_age(self, data):
+    ...         return int(data)
+    ...     def format_sex(self, data):
+    ...         return data[0]
+
+    >>> person_formatter = PersonFormatter()
+    >>> person_formatter.format(input)
+    {'age': 25, 'name': 'Will', 'sex': 'M'}
+    
+    
+    The input dict given to a Formatter is cached in the instance variable
+    'input', so it is available to all of the formatting functions.  Thus,
+    the formatting functions can change their behavior based on other values
+    in the input.  This formatter should add some text to the 'name' if the
+    given 'sex' is 'Male'.
+    
+    >>> class NameChangeFormatter(Formatter):
+    ...     def format_name(self, data):
+    ...         if self.input['sex'] == 'Male':    
+    ...             return data + ' the Man' 
+    ...         else:
+    ...             return data
+    
+    >>> name_changer = NameChangeFormatter()
+    >>> name_changer.format(input)
+    {'age': '25', 'name': 'Will the Man', 'sex': 'Male'}
+    """
 
     def __init__(self):
         self.input = None
-        self.re_dash = re.compile('-')
-        self.re_underscore = '_'
 
-    @cachedmethod
+    @cached
     def __get_format_func(self, field):
-        return 'format_%s' % re.sub(self.re_dash, self.re_underscore, field)
+        return 'format_%s' % field.replace('-', '_')
     
     def format(self, input):
         """Formats the given input according to the formatting
@@ -45,3 +97,10 @@ class Formatter:
     def post_formatter(self, value):
         """By default, return the input value unchanged."""
         return value
+
+
+if __name__ == '__main__':
+    import doctest
+    failures, tests = doctest.testmod()
+    if failures is 0:
+        print 'All tests passed.'
