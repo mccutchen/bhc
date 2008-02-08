16a24dedb96ab7b6e1e09b9c16abafa8a333e2c1
Will McCutchen <will@mccutch.org>
2008-02-08 21:06:47 +0000

    Updated to use new wrm.xmlutils.transform function to do the Saxon transformation

--- quartermaster/trunk/quartermaster.py
+++ quartermaster/trunk/quartermaster.py
@@ -1,6 +1,6 @@
-# $Id: quartermaster.py 1666 2005-11-30 16:48:10Z wrm2110 $
-
 import os, sys
+
+from wrm import xmlutils
 import xmlbuilder
 from profiles import profile
 
@@ -9,19 +9,12 @@ def main():
     xmldoc = xmlbuilder.build()
     
     # transform the XML file
-    print 'Transforming XML document...'
-    cmd = 'java -jar %s -o transform.log %s %s' % (profile.saxon_path, profile.output_xml_path, profile.template)
-    saxonin, saxonout, saxonerr = os.popen3(cmd)
+    print 'Generating Quartermaster output...'
+    xmlutils.transform(profile.output_xml_path, profile.template,
+                       profile.saxon_path)
     
-    # report any errors or status messages
-    for line in saxonerr:
-        print >> sys.stderr, ' ! Error: %s' % line.strip()
-    for line in saxonout:
-        print ' - Status: %s' % line.strip()
-
     print 'Finished.'
     
-
 
 if __name__ == '__main__':
     main()
