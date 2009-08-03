9a3c0a62f6996d67e307f14c40cbe64f5512e2c1
Travis.Haapala <Travis.Haapala@gmail.com>
2009-08-03 19:19:38 +0000

    Rewrote the x-tag extractor to display additional info. Also plopped a batch file to make it easier to run.

---
+++ son-of-schedulebuilder/trunk/data/analyzers/xtag-extractor.bat
@@ -0,0 +1,3 @@
+@python -m xtag-extractor
+@echo done.
+@pause
--- son-of-schedulebuilder/trunk/data/analyzers/xtag-extractor.py
+++ son-of-schedulebuilder/trunk/data/analyzers/xtag-extractor.py
@@ -1,27 +1,44 @@
-import re, sys
+import re, sys, glob
 
 """Looks at the data in the file named by inpath,
 which should be a Quark XPress Tags file, and extracts
 all the tags."""
 
-inpath = 'fall.txt'
-input = file(inpath).read()
+data_dir = 'xtag-extractor data/';
+data_files = glob.glob(data_dir + '*.txt');
+out_file = 'xtag report.txt';
 
-patterns = [
-    # paragraph styles
-    r'@([A-z ]+):',
+paragraph_pattern = r'@([A-z ]+):';
+inline_pattern = r'<@([A-z ]+)>';
 
-    # character styles
-    r'<@([A-z ]+)>',
-]
+paragraph_styles = [];
+inline_styles = [];
 
-styles = []
-for pattern in patterns:
-    for match in re.finditer(pattern, input):
-        style = match.group(1)
-        if style not in styles:
-            styles.append(style)
+for inpath in data_files:
+    input = file(inpath).read();
 
-styles.sort()
-for style in styles:
-    print style
+    # find paragraph styles
+    for match in re.finditer(paragraph_pattern, input):
+        style = match.group(1);
+        if style not in paragraph_styles:
+            paragraph_styles.append(style);
+
+    # find inline styles
+    for match in re.finditer(inline_pattern, input):
+        style = match.group(1);
+        if style not in inline_styles:
+            inline_styles.append(style);
+
+paragraph_styles.sort();
+inline_styles.sort();
+
+fout = file(out_file, 'w');
+
+print >> fout, "Paragraph styles:";
+for style in paragraph_styles:
+    print >> fout, style;
+
+
+print >> fout, "\nInline styles:";
+for style in inline_styles:
+    print >> fout, style;
